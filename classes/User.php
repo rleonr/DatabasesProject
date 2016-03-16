e922418a84b8cbd7797ec6ecede1ce4cb4e5b8d9
Ricardo <rleonr93@gmail.com>
2016-03-16 10:11:09 -0400

    Initial Comit

---
+++ classes/User.php
@@ -0,0 +1,15 @@
+<?php
+
+class User{
+    private $UserID;
+    private $password;
+    private $first_name;
+    private $last_name;
+    private $email;
+    private $city;
+    private $province;
+    private $country;
+    
+}
+
+?>
