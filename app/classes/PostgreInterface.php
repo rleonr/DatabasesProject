1517ad68b554d180c0c7cd8f83002289f3c9c077
Ricardo <rleonr93@gmail.com>
2016-03-27 19:57:17 -0400

    password hidden for postgre connection

--- app/classes/PostgreInterface.php
+++ app/classes/PostgreInterface.php
@@ -7,7 +7,7 @@ class PostgreInterface
     function __construct()
     {
         try{
-            $this->pdo= new \PDO('pgsql:dbname=rleon061;host=web0.site.uottawa.ca;port=15432;user=rleon061;password=rleonR-79365');
+            $this->pdo= new \PDO('pgsql:dbname=rleon061;host=web0.site.uottawa.ca;port=15432;user=rleon061;password=****');
         } catch (PDOException $e) {
              print "Error!: " . $e->getMessage() . "<br/>";
             die();
